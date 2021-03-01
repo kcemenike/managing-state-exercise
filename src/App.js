6407174b31c5076c83bf1f204bc2653c5c21b917
kcemenike <29731161+kcemenike@users.noreply.github.com>
2021-03-01 19:29:31 +0100

    solved

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Score from "./Score";
+import Game from "./Game";
 
 class App extends Component {
+  state = {
+    correctAnswer: 0,
+    numQuestions: 0,
+  };
+  // if answer is correct, increment currState by 1
+  handleAnswer = (answerWasCorrect) => {
+    if (answerWasCorrect) {
+      this.setState((currState) => ({
+        correctAnswer: currState.correctAnswer + 1,
+      }));
+    }
+    this.setState((currState) => ({
+      numQuestions: currState.numQuestions + 1,
+    }));
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
+          <h1 className="App-title">
+            How much math do you know?
+            <br />
+            You wanna give this a shot?
+          </h1>
         </header>
         <div className="game">
           <h2>Mental Math</h2>
-          <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-          </div>
-          <button>True</button>
-          <button>False</button>
-          <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
-          </p>
+          <Game handleAnswer={this.handleAnswer} />
+          <Score
+            numCorrect={this.state.correctAnswer}
+            numQuestions={this.state.numQuestions}
+          />
         </div>
       </div>
     );
